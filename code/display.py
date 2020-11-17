3fa2939990957cfa21aa57c83bc8d04e08bbdf23
Harvey <harveyjwilliams@aol.co.uk>
2020-11-17 22:02:50 +0100

    Got display to show dummy menu.

--- code/display.py
+++ code/display.py
@@ -1,5 +1,7 @@
 from machine import Pin, I2C
 from lib.ssd1306 import SSD1306_I2C
+from lib.menu import menu as m
+from lib.GFX import GFX
 
 display_rst = Pin(16, Pin.OUT)
 display_rst.value(1)
@@ -10,3 +12,7 @@ oled_width = 128
 oled_height = 64
 
 display = SSD1306_I2C(oled_width, oled_height, i2c)
+
+graphics = GFX(oled_width, oled_height, display.pixel)
+
+menu = m(graphics, display, 0, 0, 128, 64)
--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 from time import sleep
 import api
-from display import display
+from display import display, graphics, menu
 
 display.text('Starting...', 0, 0)
 
@@ -17,29 +17,38 @@ def show_health(health):
     display.show()
 
 
-def show_drinks():
+def show_drinks(drinks):
     display.fill(0)
 
     display.text('Drinks:', 0, 10)
-
-    drinks = api.get_drinks()
 
     for i, drink in enumerate(drinks, start=2):
         display.text('- ' + drink.name, 0, 10 * i)
 
     display.show()
 
-while True:
-    sleep(5)
+text = [
+    ['music','print("perform a music")'],
+    ['game','a']
+]
 
-    display.fill(0)
-    display.text('Getting health...', 0, 10)
-    display.show()
+menu.initText(text)
 
-    health = api.get_health()
-    show_health(health)
+display.show()
 
-    if health.is_healthy:
-        sleep(5)
+# while True:
+#     sleep(5)
 
-        show_drinks()
+#     display.fill(0)
+#     display.text('Getting health...', 0, 10)
+#     display.show()
+
+#     health = api.get_health()
+#     show_health(health)
+
+#     if health.is_healthy:
+#         sleep(5)
+
+#         drinks = api.get_drinks()
+
+#         show_drinks(drinks)
